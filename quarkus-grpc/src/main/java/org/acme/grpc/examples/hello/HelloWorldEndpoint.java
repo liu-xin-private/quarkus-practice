f12654a4f35b2bbf5e64072d3aa350e278717c13
jackliu <jack.liu@linecorp.com>
2022-12-01 12:20:13 +0800

    save

---
+++ quarkus-grpc/src/main/java/org/acme/grpc/examples/hello/HelloWorldEndpoint.java
@@ -0,0 +1,40 @@
+package org.acme.grpc.examples.hello;
+
+import examples.Greeter;
+import examples.GreeterGrpc;
+import examples.HelloReply;
+import examples.HelloRequest;
+import io.quarkus.grpc.GrpcClient;
+import io.smallrye.mutiny.Uni;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/hello")
+public class HelloWorldEndpoint {
+
+    @GrpcClient("hello")
+    GreeterGrpc.GreeterBlockingStub blockingHelloService;
+
+    @GrpcClient("hello")
+    Greeter helloService;
+
+    @GET
+    @Path("/blocking/{name}")
+    public String helloBlocking(String name) {
+        HelloReply reply = blockingHelloService.sayHello(HelloRequest.newBuilder().setName(name).build());
+        return generateResponse(reply);
+
+    }
+
+    @GET
+    @Path("/mutiny/{name}")
+    public Uni<String> helloMutiny(String name) {
+        return helloService.sayHello(HelloRequest.newBuilder().setName(name).build())
+                .onItem().transform((reply) -> generateResponse(reply));
+    }
+
+    public String generateResponse(HelloReply reply) {
+        return String.format("%s! HelloWorldService has been called %d number of times.", reply.getMessage(), reply.getCount());
+    }
+}
